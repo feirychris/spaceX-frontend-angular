d2b30892158329cd5f7d52a0cee361fa8c54ed2f
antoine <nogoup.antoine@gmail.com>
2021-06-17 13:52:13 +0200

    add capsule-model component and html

---
+++ src/app/models/capsules.ts
@@ -0,0 +1,14 @@
+export class Capsule {
+
+    capsule_id!: string;
+    capsule_serial!: string;
+    status!: string;
+    original_launch!: string;
+    original_launch_unix!: string;
+    missions!: [];
+    details!: string;
+    type!:string;
+    landings!: number;
+    reuse_count!: number;
+    
+}
