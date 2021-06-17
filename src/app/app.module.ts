098ae152cf728de64537a47ccf09ca474e2de7cf
Cerulean <tahir.naseer@hotmail.fr>
2021-06-17 11:17:50 +0200

    added launches component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CapsuleComponent } from './capsule/capsule.component';
+import { LaunchesComponent } from './launches/launches.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CapsuleComponent
+    CapsuleComponent,
+    LaunchesComponent
   ],
   imports: [
     BrowserModule
