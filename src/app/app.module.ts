9015fedccef241a29fb6e7b7770459c80d35aea8
Cerulean <tahir.naseer@hotmail.fr>
2021-06-17 11:16:30 +0200

    added capsule component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CapsuleComponent } from './capsule/capsule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CapsuleComponent
   ],
   imports: [
     BrowserModule
